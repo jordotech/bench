e003fbf77d7a5334df127426e2b9927bc3d9b543
jordotech <jaustin@proace.com>
2013-06-13 16:17:56 -0500

    template directory

---
+++ BB_information_files/AMC_update_05-08-13/assets/js/script.js
@@ -0,0 +1,35 @@
+/* Company
+ * ..........................................................................
+ * http://www.company.com/
+ * designed by Switch Creative Group
+ * http://www.groupswitch.com
+*/
+
+
+$(document).ready(function(){
+	
+	
+		
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
